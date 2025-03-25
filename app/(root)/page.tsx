584c04825d7bd519407f0faac06fd0ae8cfb3c23
prabhanjansp <puranikchetan97@gmail.com>
2025-03-25 23:34:47 +0530

    day js package installation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Page = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg ">
+          <h2>Get interview ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview question & get instant feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interviews">Start an Interview</Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          height={400}
+          width={400}
+          className="max-sm:hidden "
+        />
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
 
-export default Page
+        <div className="interview-section">
+          <p>You have not taken any interviews yet</p>
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an interview</h2>
+        <div className="interviews-section">
+          <p>There are no interview available</p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Page;
